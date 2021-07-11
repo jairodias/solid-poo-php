1547719054d86e05f1a3ff4d84434a9b52005f29
jairodias <jairo.junior@credpago.com>
2021-07-11 10:37:14 -0300

    refactor: removing the old folder

--- app/index.php
+++ app/index.php
@@ -2,14 +2,34 @@
 
 require  __DIR__ . "/vendor/autoload.php";
 
-use App\CarrinhoCompra;
 
-$cart = new CarrinhoCompra();
+use App\Cart;
+use App\Item;
+use App\Order;
+use App\EmailService;
 
-var_dump($cart->getItems());
+$order = new Order;
 
-$cart->addItemToCart('Bicicleta', 1500);
-$cart->addItemToCart('Tapete', 150.20);
+$itemOne = new Item();
+$itemTwo = new Item();
+
+$itemOne->setDescription('Porta Copos');
+$itemOne->setValue(4.55);
+
+$itemTwo->setDescription('Ventilador');
+$itemTwo->setValue(189.83);
 
 echo "<pre>";
-var_dump($cart->getItems());
+echo '<h6>Pedido</h6>';
+print_r($order);
+
+$order->getCart()->addItemToCart($itemOne);
+$order->getCart()->addItemToCart($itemTwo);
+
+echo '<h6>Pedido itens</h6>';
+print_r($order->getCart()->getItems());
+
+echo '<h6>Pedido itens</h6>';
+print_r($order->getCart()->getItems());
+
+$cart = new Cart;
