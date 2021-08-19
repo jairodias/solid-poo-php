482d0e54c1e1f0ad93f02cfce9873e5429aa46d2
Jairo Dias <jairopereira_dias@hotmail.com>
2021-08-19 18:49:00 -0300

    OCP - SOLID

--- app_etl/src/Leitor.php
+++ app_etl/src/Leitor.php
@@ -32,17 +32,18 @@ class Leitor
     public function lerArquivo(): array
     {
         $caminho = $this->getDiretorio() . "/" . $this->getArquivo();
-
-        $arquivo = new Arquivo();
-
         $extensao = explode('.', $this->getArquivo());
 
-        if ($extensao[1] == 'csv') {
-            $arquivo->lerArquivoCSV($caminho);
-        } else if ($extensao[1] == 'txt') {
-            $arquivo->lerArquivoTXT($caminho);
-        }
+        $classe = '\src\extrator\\' . ucfirst($extensao[1]);
 
-        return $arquivo->getDados();
+        return call_user_func_array(
+            [
+                new $classe,
+                'lerArquivo'
+            ],
+            [
+                $caminho
+            ]
+        );
     }
 }
---
+++ app_etl/src/extrator/Txt.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace src\extrator;
+
+use src\extrator\Arquivo;
+
+class Txt extends Arquivo
+{
+    public function lerArquivo(string $caminho): array
+    {
+        $handle = fopen($caminho, 'r');
+        while (!feof($handle)) {
+            $linha = fgets($handle);
+
+            $this->setDados(
+                substr($linha, 11, 30),
+                substr($linha, 0, 11),
+                trim(substr($linha, 41, 50)),
+            );
+        }
+
+        fclose($handle);
+
+        return $this->getDados();
+    }
+}
